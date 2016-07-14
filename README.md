75b992ea19b713a44e282872a79d75e69ec77043
Anne Archibald <archibald@astron.nl>
2016-07-15 00:37:47 +0200

    Initial setup and basic code.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # weighted_photon_tools
-Tools for working with probability-weighted photons
+
+Tools for working with probability-weighted photons.
+
+If appropriately appeased by offerings (for vegetarians we suggest asparagus), the Fermi tools can be made to yield a photon list in which each photon is tagged with the probability of it being from your object of interest. This takes the place of aperture photometry as a way of distinguishing source photons from background, though it should be noted that many of these probabilities are rather small, and unfortunately even much of the total probabilty comes from low-probability photons. It is thus imperative to process these photons with tools that correctly take into account these probability weights.
+
+In the literature you will find that Matthew Kerr has worked out the correct way to compute an H test for such photons. This toolkit includes that, of course, but also includes tools for histogramming and background subtraction. This toolkit is also a work in progress, and will probably acquire new functionality and new interfaces, including probably a Profile object for working with sets of empirical Fourier coefficients (a folded pulse profile in Fourier form).
+
+
---
+++ weighted_photon_tools.py
@@ -0,0 +1,79 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import division, print_function
+
+import numpy as np
+
+def h_fpp(H):
+    return np.exp(-0.398405*H)
+def log10_h_fpp(H):
+    return -0.398405*H/np.log(10)
+def empirical_fourier(phases, probs=None, n=20):
+    if probs is not None:
+        coeff = np.sum(
+            probs*np.exp(-2.j*np.pi*(np.arange(n+1)[:,None])*phases), axis=1)
+    else:
+        coeff = np.sum(
+            np.exp(-2.j*np.pi*(np.arange(n+1)[:,None])*phases), axis=1)
+    return coeff
+def reconstruct(efc, xs):
+    efcp = np.asarray(efc, dtype=np.complex)
+    efcp[1:] *= 2
+    return np.sum(efc*np.exp(2.j*np.pi*xs[:,None]*np.arange(len(efc))),
+                      axis=1).real
+def h_raw(efc, probs=None):
+    coeff = efc[1:]
+    if probs is not None:
+        tpsq = np.sum(probs**2)
+    else:
+        tpsq = 1
+    k = 2*np.abs(coeff)**2/tpsq
+    hs = np.cumsum(k)-4*np.arange(len(k))
+    ix = np.argmax(hs)
+    return hs[ix], ix+1
+def h(phases, probs=None, n=20):
+    return log10_h_fpp(h_raw(empirical_fourier(phases, probs, n),probs)[0])
+
+def weighted_histogram_errors(phases, probs=None, bins=20, middle=0):
+    if probs is None:
+        probs = np.ones_like(phases)
+    phases = (phases-(middle-0.5)) % 1 + (middle-0.5)
+    prob, be = np.histogram(phases, bins=bins, range=(middle-0.5,middle+0.5),
+                             weights=probs)
+    prob_sq, be = np.histogram(phases, bins=bins, range=(middle-0.5,middle+0.5),
+                             weights=probs**2)
+    prob_uncert = np.sqrt(prob_sq)
+
+    return prob, prob_uncert, be
+
+def plot_result(phases, probs=None, middle=0,
+                nharm='best', bins='best',
+                time=1.):
+    import matplotlib.pyplot as plt
+    efc = empirical_fourier(phases, probs)
+    hr, nc = h_raw(efc, probs)
+    H = log10_h_fpp(hr)
+    if nharm is None or nharm=='best':
+        nharm = nc
+    if bins is None or bins=='best':
+        bins = 4*nharm
+    if probs is not None:
+        bglevel = np.sum(probs*(1-probs))/time
+    else:
+        bglevel = 0
+    prob, prob_uncert, be = weighted_histogram_errors(phases, probs, bins,
+                                                          middle=middle)
+    plt.plot(np.repeat(be,2)[1:-1], np.repeat(prob,2)*bins/time-bglevel)
+    plt.errorbar((be[1:]+be[:-1])/2,
+                     prob*bins/time-bglevel,
+                     prob_uncert*bins/time,
+                     linestyle='none')
+
+    xs = np.linspace(middle-0.5,middle+0.5,16*nharm+1)
+    ys = reconstruct(efc[:nharm+1], xs)
+    plt.plot(xs,ys/time-bglevel)
+
+    plt.xlim(middle-0.5, middle+0.5)
+
+    return H, bins
+
